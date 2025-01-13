de969ce72a45a6cfd01d23d302b1db9ff15a2871
okino-shogo <okisho0106@gmail.com>
2025-01-13 14:39:22 +0900

    計算アプリ課題完了

---
+++ app/Http/Controllers/CalcController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalcController extends Controller
+{
+    public function calcs($i1, $operator, $i2)
+    {
+        if ($operator == 'addition') {
+            $result = $i1 + $i2;
+        } elseif ($operator == 'subtraction') {
+            $result = $i1 - $i2;
+        } elseif ($operator == 'multiplication') {
+            $result = $i1 * $i2;
+        } else {
+            $result = $i1 / $i2;
+        }
+        return view('calcs.result1', ['result' => $result]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CalcController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/calcs/{i1}/{operator}/{i2}', [CalcController::class, 'calcs']);
